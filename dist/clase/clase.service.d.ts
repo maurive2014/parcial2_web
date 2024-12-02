de9078802a68713882cc23cc23feddfccbcbf8df
maurive2014 <o.urregov@uniandes.edu.co>
2024-12-01 22:55:42 -0500

    ignore dist

--- dist/clase/clase.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { ClaseEntity } from './clase.entity';
-export declare class ClaseService {
-    private readonly claseRepository;
-    constructor(claseRepository: Repository<ClaseEntity>);
-    crearClase(clase: ClaseEntity): Promise<ClaseEntity>;
-    findClaseById(id: string): Promise<ClaseEntity>;
-}
--- dist/usuario/usuario.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class UsuarioDto {
-    readonly nombre: string;
-    readonly rol: string;
-    readonly correo: string;
-    readonly cedula: number;
-    readonly grupodeinvestigacion: string;
-    readonly numeroextension: number;
-}
